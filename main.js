f0ec03a3d1a308356736ebc680cbe14789234026
Anya-Mo <72193720+Anya-Mo@users.noreply.github.com>
2021-03-28 19:10:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+car1_width=120;
+car1_height=70;
+car1_image="car1.png";
+car1_x=10;
+car1_y=10;
+car2_width=120;
+car2_height=70;
+car2_image="car2.png";
+car2_x=10;
+car2_y=10;
+var background_imgTag="Background.gif";
+function add() {
+    background_imgTag=new Image();
+    background_imgTag=uploadBackground;
+    background_imgTag=background_image;
+    car_1imgTag=new Image();
+    car_1imgTag.onload=uploadcar1;
+    car_1imgTag.src="car1.png";
+    car_2imgTag=new Image();
+    car_2imgTag.onload=uploadcar1;
+    car_2imgTag.src="car2.png";
+}
